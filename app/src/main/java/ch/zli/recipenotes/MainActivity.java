9502ca78d1d8f83b814c8385850ec0aff52ba2a9
TechMasterIvan <49059482+TechMasterIvan@users.noreply.github.com>
2022-07-08 16:32:37 +0200

    Camera functionality Image preview Logo XML

--- app/src/main/java/ch/zli/recipenotes/MainActivity.java
+++ app/src/main/java/ch/zli/recipenotes/MainActivity.java
@@ -2,13 +2,26 @@ package ch.zli.recipenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_plus);
+        button.setOnClickListener(v -> openCreateRecipeActivity());
+    }
+
+    public void openCreateRecipeActivity() {
+        Intent intent = new Intent(this, CreateRecipeActivity.class);
+        startActivity(intent);
     }
 }
